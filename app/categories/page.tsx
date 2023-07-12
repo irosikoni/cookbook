94eb7257c8b12e8287182a27d6d80ea545325702
iza <irosikoni@gmail.com>
2023-07-13 01:25:56 +0200

    CategoryButton element created

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,3 +1,5 @@
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
+
 type Product = {
     id: number;
     title: string;
@@ -5,6 +7,20 @@ type Product = {
     images: string[]
 }
 
+
+function CategoryButton({ item }: { item: { images: (string | undefined)[]; title: string | null | undefined; }; }) {
+
+    return (
+        <div>
+            <img
+                src={item.images[0]}
+                width="300px"
+            />
+            <h5>{item.title}</h5>
+        </div>
+    )
+
+}
 
 export default async function Categories() {
 
@@ -15,7 +31,17 @@ export default async function Categories() {
     return (
         <div>
             <div>Categories</div>
-            <ul>{products.map(product => <li key={product.id}>{product.title}</li>)}</ul>
+            <ul>{products.map(product => <li key={product.id}>
+                <CategoryButton
+
+                    item={{
+                        title: product.title,
+                        images: product.images,
+                    }}
+                />
+            </li>
+            )}
+            </ul>
         </div>
     )
 }
