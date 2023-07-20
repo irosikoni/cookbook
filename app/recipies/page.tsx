fe9a15b03cfaf7d66351b5ccfe3e70fb68aeb40c
iza <irosikoni@gmail.com>
2023-07-20 13:59:11 +0200

    fixes from comments in pull request

--- app/recipies/page.tsx
+++ app/recipies/page.tsx
@@ -1,59 +1,55 @@
 import Link from "next/link";
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
 
 type Product = {
+  id: number;
+  title: string;
+  description: string;
+  images: string[];
+};
+
+function CategoryButton({
+  item,
+}: {
+  item: {
     id: number;
-    title: string;
-    description: string;
-    images: string[]
-}
-
-
-function CategoryButton({ item }: { item: { id: number; images: (string | undefined)[]; title: string | null | undefined; }; }) {
-
-    return (
-
-        <div className="h-72 bg-cupcake-light-grey flex flex-col justify-items-center content-around items-center justify-center w-60">
-            <div className="m-5 h-48 w-48 overflow-hidden">
-
-                <img
-                    src={item.images[0]}
-                />
-            </div>
-            <div>
-
-                <h5>{item.title}</h5>
-            </div>
-            <div></div>
-        </div>
-
-    )
-
+    images: (string | undefined)[];
+    title: string | null | undefined;
+  };
+}) {
+  return (
+    <div className="h-72 bg-cupcake-light-grey flex flex-col justify-items-center content-around items-center justify-center w-60">
+      <div className="m-5 h-48 w-48 overflow-hidden">
+        <img src={item.images[0]} />
+      </div>
+      <div>
+        <h5>{item.title}</h5>
+      </div>
+    </div>
+  );
 }
 
 export default async function Categories() {
-
-    const response = await fetch('https://dummyjson.com/products')
-    const data = await response.json() as { products: Product[] }
-    const { products } = data
-    console.log(data)
-    return (
-        <div>
-            <div>Categories</div>
-            <div className="grid grid-cols-3 gap-4 justify-items-center">
-                {products.map(product => <div key={product.id}>
-                    <Link href={`/recipies/${product.id}`}>
-                        <CategoryButton
-                            item={{
-                                id: product.id,
-                                title: product.title,
-                                images: product.images,
-                            }}
-                        />
-                    </Link>
-                </div>
-                )}
-            </div>
-        </div>
-    )
+  const response = await fetch("https://dummyjson.com/products");
+  const data = (await response.json()) as { products: Product[] };
+  const { products } = data;
+  return (
+    <div>
+      <div>Categories</div>
+      <div className="grid grid-cols-3 gap-4 justify-items-center">
+        {products.map((product) => (
+          <div key={product.id}>
+            <Link href={`/recipies/${product.id}`}>
+              <CategoryButton
+                item={{
+                  id: product.id,
+                  title: product.title,
+                  images: product.images,
+                }}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
