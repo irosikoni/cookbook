99a2db2d1bb92367cc00a9b31ad6cb89e47cddc9
iza <irosikoni@gmail.com>
2023-08-28 01:03:39 +0200

    post removed

--- app/api/recipes/route.ts
+++ app/api/recipes/route.ts
@@ -5,11 +5,3 @@ export async function GET(req: NextRequest) {
   const recipes = await prisma.recipe.findMany();
   return NextResponse.json(recipes);
 }
-
-export async function POST(req: NextRequest) {
-  const json = await req.json();
-  const recipe = await prisma.recipe.create({
-    data: json,
-  });
-  return NextResponse.json(recipe);
-}
